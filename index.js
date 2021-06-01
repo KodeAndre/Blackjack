e241632709ee1996d2cd7e78b4f527147ad8e522
anarchytrex <anarchytrex@hotmail.com>
2021-06-01 20:33:50 +0200

    v1

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+let player = {
+    name: "Echo",
+    chips: 145
+}
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
 function getRandomCard() {
     let randomNumber = Math.floor(Math.random() * 13) + 1;
     if (randomNumber > 11) {
